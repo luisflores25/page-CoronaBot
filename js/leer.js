a4063a7b86f74ef2f34d6ee0dc7f349bc189eea7
LALECHERA <regina-blue@hotmail.com>
2020-08-15 19:36:11 -0500

    texto
    texto

--- js/leer.js
+++ js/leer.js
@@ -16,41 +16,44 @@
 // }
 
 var a = 0
-var n1 = document.getElementById('n1')
-var n2 = document.getElementById('n2')
+var n1 = document.getElementById('text1')
+var n2 = document.getElementById('text2')
 
 
 
-//window.onload = n1.slideUp(); n2.slideUp();
+//window.onload = n1.hide(); n2.hide();
+n1.hidden = true
+n2.hidden = true
 
-
+console.log(n1)
 
 function yeah(n) {
+    console.log(n)
     switch (n) {
-        case 1:
-            if (!a) {
-                n1.slideDown();
-                document.getElementById('btn').innerHTML = "READ LESS";
+         case 1:
+            if (a == 0) {
+                n1.hidden = false;
+                document.getElementById('btn1').innerHTML = "MOSTRAR MENOS";
                 a = 1;
             }
             else {
-                n1.slideUp();
-                document.getElementById('btn').innerHTML = "READ MORE";
+                n1.hidden = true;
+                document.getElementById('btn1').innerHTML = "LEER MÁS";
                 a = 0;
             }
             break;
 
         case 2:
-            if (!a) {
-                n2.slideDown();
-                document.getElementById('btn2').innerHTML = "READ LESS";
+            if (a == 0) {
+                n2.hidden = false;
+                document.getElementById('btn2').innerHTML = "MOSTRAR MENOS";
                 a = 1;
             }
             else {
-                n2.slideUp();
-                document.getElementById('btn2').innerHTML = "READ MORE";
+                n2.hidden = true;
+                document.getElementById('btn2').innerHTML = "LEER MÁS";
                 a = 0;
             }
             break;
     }
-}
+ }
